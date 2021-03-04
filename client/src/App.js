fb1cf356f46e08f2f13282b7c4b53d376c5201cd
Tekena Solomon <tekena157@gmail.com>
2021-03-04 10:27:11 +0100

    Setup app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="speech-text-container">
+        <h1>Speech to text</h1>
+      </div>
+      <div className="vertical-line"></div>
+      <div className="text-speech-container">
+        <h1>Text to speech</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
